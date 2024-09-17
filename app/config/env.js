66e00a5e28695afe04dfcf5b7824c66f8a315bc4
JocoGar <1josejoaquin1@gmail.com>
2024-09-17 16:45:39 -0600

    very lastone

--- app/config/env.js
+++ app/config/env.js
@@ -1,5 +1,5 @@
 const env={
-    host: 'dpg-cr6jn5qj1k6c73d5eps0-a.oregon-postgres.render.com',
+    host: 'dpg-cr6jn5qj1k6c73d5eps0-a',
     port: 5432,
     username: 'antigua_umg2024_10867_dtdb_user',
     password: 'dU5Bdxoe42cFfbeRAJxVuFnU5In7tbG2',
